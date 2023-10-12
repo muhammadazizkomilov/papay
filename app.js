919344784e26863da936228f6b9f75e5ae3402e7
muhammadazizkomilov <komilov.mukhammadaziz@gmail.com>
2023-10-12 18:07:30 +0900

    feat: dotenv & project configratsiyasi

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 // const res = require("express/lib/response")
 const app = express();
 //const http = require("http");
-const fs = require("fs");
 // mongono chaqirish;
 const db = require("./server").db();
 // bu db orqali databasega turli xilmalumotlarni yozish uqish uchurish yoki delete qilishni.
@@ -35,38 +34,5 @@ app.set("views","views");
 app.set("view engine","ejs");
 
 //--------------------------4-----------------//
-app.post("/create-item", (req, res) => {
-   // console.log("user entered /create-item");
-   console.log(req.body);
-   const new_reja = req.body.reja;
-   db.collection("plans").insertOne({reja: new_reja}, (err,data) => {
-    console.log(data.ops);
-    res.json(data.ops[0]);
-    // if(err) {
-    //     console.log(err);
-    //     res.end("something went wrong")
-    // } else {
-    //     res.end("successfully added");
-    // }
-   });
-   //res.end("success") ;                                  // bu yerda boddyni tekshiri
- //res.json({ test: "success"});
-});
-
-
-     app.get("/",function (req, res) {
-      console.log("user entered /create-item");
-      db.collection("plans")
-      .find()
-      .toArray((err, data) => {
-       if (err) {
-        console.log(err);
-        res.end("something went wrong");
-       } else {
-        //console.log(data);
-         res.render("reja",{items: data});
-    }
-   });
-  });
 
     module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config()
+
 const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString =
-  "mongodb+srv://danny:CdWzaqRFEtB8R9Et@cluster0.ar5nuyp.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = process.env.MONGO_URL;
+  
+  
 
 mongodb.connect(connectionString, {
   useNewUrlParser: true,
@@ -19,7 +23,7 @@ mongodb.connect(connectionString, {
                     // process.env.PORT || 
     const app = require("./app");
     const server = http.createServer(app);
-    let PORT =3003;
+    let PORT =process.env.PORT || 3003;
     server.listen(PORT, function () {
       console.log(
         `Server muvaffaqiyatli ishlayapti, PORT: ${PORT}, http://localhost:${PORT})`
